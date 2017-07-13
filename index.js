bf75cdff6f771a9092fa0a6c734f3aa7db10da7b
TRI VI <trivi2710@gmail.com>
2017-07-13 12:31:40 -0400

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 var multer = require('multer');
 var storage = multer.diskStorage({
 	destination: function(req, file, cb) {
-		cb(null, '/public/img/upload')
+		cb(null, 'public/img/upload')
 	},
 	filename: function(req, file, cb) {
 		cb(null, Date.now() + "-" + file.originalname);
@@ -32,9 +32,31 @@ app.listen(3000, function (err){
 })
 
 app.get('/', function (req, res){
-	res.render('mainpage');
+	res.render('mainpage', {data: data});
 });
 
 app.get('/admin', function (req, res){
 	res.render('addItem');
-})
+});
+
+app.post('/upload', urlencodedParser, function (req, res){
+	upload(req, res, function (err){
+		if(err) {
+			console.log(err);
+		}
+		var title = req.body.title;
+		var description = req.body.description;
+		var image = req.file.filename;
+
+		data.push(new Task(title, description, image));
+		res.redirect('/');
+	})
+});
+
+var data = [];
+
+var Task = function(title,description, image) {
+	this.title = title,
+	this.description = description,
+	this.image = image;
+}
